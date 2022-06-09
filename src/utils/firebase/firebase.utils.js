283217658184db90c2fc8ccf472111bfb2f63744
emosturi <nicsalb@gmail.com>
2022-06-09 19:02:46 -0400

    primer commit para un nuevo comienzo

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyB9PjfmuaqHF2jkRZRFfkowVdLAvfoYioE",
+  authDomain: "jack-n-jones.firebaseapp.com",
+  projectId: "jack-n-jones",
+  storageBucket: "jack-n-jones.appspot.com",
+  messagingSenderId: "406919391300",
+  appId: "1:406919391300:web:ec295675e2d345440ef411"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
